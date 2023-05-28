4d925455b73ee5c9b3b6b30206ebff94769488ca
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-05-28 23:10:40 +0000

    pipeline for Z2 training (up to LLaMA-13B)

---
+++ scripts/slurm_train_deepspeed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=trlx
+#SBATCH --nodes=1
+#SBATCH --cpus-per-gpu=10
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=compute
+#SBATCH --mem=800G
+#SBATCH --output=logs/%x_%j.out
+
+export WANDB__SERVICE_WAIT=300
+source /opt/rh/devtoolset-10/enable
+
+# just pass all the arguments forward
+python "$@"
